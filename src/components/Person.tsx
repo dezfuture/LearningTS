9345fe8f96863c203c3d3884b570af0640f83e63
dezfuture <63505820+dezfuture@users.noreply.github.com>
2023-04-09 03:39:41 +0530

    how to use useState and input in typescript

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, ChangeEvent, useState} from 'react';
 
 interface Props {
     name: string;
@@ -7,11 +7,24 @@ interface Props {
 }
 
 export const Person: FC<Props> = ({name,age,email}) => {
+  
+  const [country, setCountry] = useState<string | null>("")
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value)
+  }
+  
   return (
     <div>
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+
+      <input placeholder='Write down your country...' onChange={handleChange} />
+      <br/>
+
+      <h1>{country}</h1>
+
     </div>
   );
 }
